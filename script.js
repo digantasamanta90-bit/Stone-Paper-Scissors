45106be74da6425096fde4f615cd1b4abc4b1a3e
Diganta <digantasamanta90@gmail.com>
2026-09-15 23:00:01 +0530

    feat: enhance UI with interactive battle arena, reset controls, and responsive styling

--- script.js
+++ script.js
@@ -2,83 +2,111 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
-
 const msg = document.querySelector("#msg");
-
+const resultBadge = document.querySelector("#result-badge");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
+const userSlot = document.querySelector("#user-slot");
+const compSlot = document.querySelector("#comp-slot");
+const userMoveName = document.querySelector("#user-move-name");
+const compMoveName = document.querySelector("#comp-move-name");
+const resetBtn = document.querySelector("#reset-btn");
 
+const choiceIcons = {
+    rock: "fa-hand-back-fist",
+    paper: "fa-hand",
+    scissors: "fa-hand-scissors"
+};
+
+const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 const genCompChoice = () => {
-    const options = ["rock","paper","scissors"];
-    const randIdx = Math.floor(Math.random()*3);
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
 
-const drawGame = () => {
-    msg.innerText = "The game was draw. Play again"
-    msg.style.backgroundColor = "gray"
-}
+const updateDuelArena = (userChoice, compChoice) => {
+    userSlot.innerHTML = `<i class="fa-solid ${choiceIcons[userChoice]}"></i>`;
+    compSlot.innerHTML = `<i class="fa-solid ${choiceIcons[compChoice]}"></i>`;
+    userMoveName.innerText = userChoice.toUpperCase();
+    compMoveName.innerText = compChoice.toUpperCase();
+
+    // Trigger pop animation
+    userSlot.classList.remove("pop");
+    compSlot.classList.remove("pop");
+    void userSlot.offsetWidth;
+    userSlot.classList.add("pop");
+    compSlot.classList.add("pop");
+};
+
+const drawGame = (userChoice) => {
+    resultBadge.innerText = "IT'S A DRAW!";
+    resultBadge.className = "result-badge draw";
+    msg.innerText = `Both picked ${capitalize(userChoice)}. No points awarded!`;
+};
 
 const showWinner = (userWin, userChoice, compChoice) => {
-    if(userWin) {
+    if (userWin) {
         userScore++;
         userScorePara.innerText = userScore;
-        console.log("you win");
-        msg.innerText = `You won! your chose ${userChoice} and the computer chose ${compChoice}`;
-        msg.style.backgroundColor = "green"
-    }else{
+        resultBadge.innerText = "YOU WIN!";
+        resultBadge.className = "result-badge win";
+        msg.innerText = `${capitalize(userChoice)} beats ${compChoice}! Point to you.`;
+    } else {
         compScore++;
         compScorePara.innerText = compScore;
-        console.log("you lost");
-        msg.innerText = `You lost! your chose ${compChoice} and the computer chose ${userChoice}`;
-        msg.style.backgroundColor = "red"
+        resultBadge.innerText = "YOU LOSE!";
+        resultBadge.className = "result-badge lose";
+        msg.innerText = `${capitalize(compChoice)} beats ${userChoice}. Computer scores!`;
     }
-}
-
-// const playGame = (userChoice) => {
-//     console.log("user choice =", userChoice);
-    
-// }
-
-// choices.forEach((choice) => {
-//     console.log(choice);
-//     choice.addEventListener("click", () =>{
-//         const choiceId = choice.getAttribute("id");
-//         console.log("choice was clicked",choiceId);
-//         playGame(userChoice); 
-//     })
-    
-// });
+};
 
 const playGame = (userChoice) => {
-    console.log("user choice =", userChoice);
     const compChoice = genCompChoice();
-    console.log("comp choice= ", compChoice);
-    
-    if (userChoice === compChoice){
-        drawGame();
-        console.log("The game is draw");
-        
-    }else{
+
+    updateDuelArena(userChoice, compChoice);
+
+    if (userChoice === compChoice) {
+        drawGame(userChoice);
+    } else {
         let userWin = true;
-        if (userChoice==="rock"){
-            userWin = compChoice === "paper" ? false:true; 
-        }else if(userChoice==="paper"){
-            userWin = compChoice === "scissors" ? false:true;
-        }else{
-            userWin = compChoice === "rock" ? false:true;
-        }      
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
         showWinner(userWin, userChoice, compChoice);
-    } 
-    
-}
+    }
+};
+
+const resetGame = () => {
+    userScore = 0;
+    compScore = 0;
+    userScorePara.innerText = "0";
+    compScorePara.innerText = "0";
+
+    userSlot.innerHTML = `<i class="fa-solid fa-question"></i>`;
+    compSlot.innerHTML = `<i class="fa-solid fa-question"></i>`;
+    userMoveName.innerText = "-";
+    compMoveName.innerText = "-";
+
+    resultBadge.innerText = "Choose your move to begin!";
+    resultBadge.className = "result-badge";
+    msg.innerText = "First to score wins the round.";
+};
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
-        // console.log("choice was clicked", choiceId);
         playGame(userChoice);
     });
 });
 
+if (resetBtn) {
+    resetBtn.addEventListener("click", resetGame);
+}
